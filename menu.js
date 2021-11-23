1a66bba9f00f6aa3183974ae915cbe05ba904043
Connor Csoky <connorcsoky@gmail.com>
2021-11-23 09:40:05 -0500

    added new features for the buttons on the menu page

---
+++ menu.js
@@ -0,0 +1,11 @@
+$('.plusButton').click(function(){
+    $('.bg-modal').css('display', 'flex');
+})
+
+$('.btnClose').click(function(){
+    $('.bg-modal').css('display', 'none');
+})
+
+$('.btnOrder').click(function(){
+    $('.orderInput').val(" ");
+})
