5bbfa7059c2b193fab7d7ddb6848d261f68f7908
ECRAMi <mcaryon2@hotmail.com>
2021-12-08 15:25:08 -0500

    Various changes

--- receipt.js
+++ receipt.js
@@ -27,4 +27,10 @@ $(document).ready(function(){
         `)
     }
 
+
+    $('.receiptClear').click(function() {
+        localStorage.setItem('receiptArr', '[]');
+        page = [];
+        $('.receipt').remove()
+    })
 });
